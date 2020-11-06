cd501b5b5c53cea5534236f743339c039b749455
maxiadlovskii <max.iadlovskii@gmail.com>
2020-11-06 13:22:52 +0200

    feat: setup project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import {CarsList} from "./components";
+
+function App() {
+  return (
+    <div className="App">
+        <CarsList />
+    </div>
+  );
+}
+
+export default App;
